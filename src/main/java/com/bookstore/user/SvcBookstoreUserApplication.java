d8ee20fd926ad68250fee42dc7e3b6edc52fb402
svogeti <sameervogeti@gmail.com>
2020-09-16 15:20:24 +0530

    Template for svc-bookstore-user

---
+++ src/main/java/com/bookstore/user/SvcBookstoreUserApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcBookstoreUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcBookstoreUserApplication.class, args);
+	}
+
+}
